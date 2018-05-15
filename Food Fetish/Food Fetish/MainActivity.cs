062a02d2539f5218acf73ff9c8db0d34d788f67e
0928136 <0928136@hr.nl>
2018-05-15 09:59:09 +0200

    -Added the app beginning settings.

---
+++ Food Fetish/Food Fetish/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.Content;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.OS;
+
+namespace Food_Fetish
+{
+    [Activity(Label = "Food_Fetish", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            //Get the ID's from the layout
+            Button settingsButton = FindViewById<Button>(Resource.Id.settingsBtn);
+            settingsButton.Click += delegate
+            {
+                SetContentView(Resource.Layout.Settings);
+            };
+        }
+    }
+}
+
